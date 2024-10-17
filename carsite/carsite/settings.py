9936022da30150f9b9e4d30ac18fbc289eddbdd2
bekturich <bekosaa@mail.ru>
2024-10-18 01:07:52 +0600

    Docker

--- carsite/carsite/settings.py
+++ carsite/carsite/settings.py
@@ -142,6 +142,8 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
